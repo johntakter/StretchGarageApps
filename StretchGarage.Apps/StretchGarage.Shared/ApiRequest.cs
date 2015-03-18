2f7d3b5cdfc49f89fd2a29a8f8660cdba064b413
johntakter <john.takter@gmail.com>
2015-03-18 14:16:30 +0100

    Changed Naming of ApiRequest class and also specified methods for creating unit and getting intervall

---
+++ StretchGarage.Apps/StretchGarage.Shared/ApiRequest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StretchGarage.Shared
+{
+    public class ApiRequest
+    {
+
+        public async static Task<object> GetUnitId()
+        {
+            string url = "http://stretchgarageweb.azurewebsites.net/api/ParkingPlace/0";
+
+            var content = await GetContent(url);
+            return content;
+        }
+        public async static Task<object> GetInterval()
+        {
+            string url = "http://stretchgarageweb.azurewebsites.net/api/Unit/";
+
+            var content = await GetContent(url);
+            return content;
+        }
+
+        async static Task<object> GetContent(string url)
+        {
+            // Create an HTTP web request using the URL:
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            // Send the request to the server and wait for the response:
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                // Get a stream representation of the HTTP web response:
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        var content = reader.ReadToEnd();
+                        return content;
+                    }
+
+                    // Use this stream to build a JSON document object:
+                    //JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    //Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+                    // Return the JSON document:
+                    //return jsonDoc;
+                }
+            }
+        }
+
+
+    }
+}
